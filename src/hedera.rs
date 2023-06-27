df06cfddd5943930e90d3ac520c6cafb17b5aa75
Nigel <niko.didic@gmail.com>
2023-06-27 20:13:56 +0200

    removed hbar transfer

--- src/hedera.rs
+++ src/hedera.rs
@@ -21,10 +21,8 @@ pub fn create_transfer_transaction(token_id: String,
     TransferTransaction::new()
         .node_account_ids(vec![AccountId::from(3)])
         .transaction_id(TransactionId::generate(payer_id))
-        .token_transfer(token_id, owner_id, -(token_amount * 10^8))
-        .token_transfer(token_id, payer_id, (token_amount * 10^8))
-        .hbar_transfer(owner_id, Hbar::new(hbar_amount))
-        .hbar_transfer(payer_id, Hbar::new(-hbar_amount))
+        .token_transfer(token_id, owner_id, -token_amount)
+        .token_transfer(token_id, payer_id, token_amount)
         .freeze_with(&*CLIENT).unwrap().sign_with_operator(&CLIENT).unwrap()
         .to_bytes()
 }
